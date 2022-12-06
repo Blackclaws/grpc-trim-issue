d9734a23b1f872ac9dc4abd5e589e033f1d5f45d
Felix Winterhalter <felix@arfinity.io>
2022-12-06 13:54:13 +0100

    Show issue

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddGrpcClient<Test.TestClient>(op =>
+{
+    op.Address = new Uri("http://localhost:8888");
+});
+
+var app = builder.Build();
+
+var client = app.Services.GetRequiredService<Test.TestClient>();
+
+var res = await client.TestAsync(new Empty());
